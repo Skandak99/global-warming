44878ea66794273c6f6a7f23ffc45575f2b6c985
Skandak99 <69813418+Skandak99@users.noreply.github.com>
2020-11-22 09:54:28 +0530

    Add files via upload

---
+++ effect.js
@@ -0,0 +1,38 @@
+var facts = [
+   
+    
+    "This is just there to fill space, lol, XD...", 
+    // lolol, cause arrays start from 0, i just added one and in the for loop it like starts from i=1 instead of i=0;
+    "Melting of Glaciers",
+    "Climate Change",
+    "Rise in the number of droughts that take place",
+    "Diseases are More likely to spread",
+    "The Frequency of a Hurricane  will increase",
+    "Rise of Sea Levels",
+    "Harder Conditions for plants to, which is bad as they are our only source of food",
+    "Unexpected Heat Waves, and record breaking temperatures",
+    "Frequent Wildfires",
+    "Severe Precipitation",
+    "Rise in Ocean Temperature",
+    "Disruption of the entire food chain",
+    "Extinction, there have been 5 already, the 6th may come thanks to global warming",
+    "Poor Air Quality",
+    "Dimishing Fresh water supplies",
+  
+    
+
+    
+];
+   var full = ". "     
+   
+		for (i=1;i<facts.length;i++)
+	{
+    document.write((i)+(full)+facts[i] + "<br />");
+    
+    }
+
+
+
+
+
+
---
+++ quiz-start.js
@@ -0,0 +1,15 @@
+const gb=document.getElementById("imggb");
+const sol=document.getElementById("imgsol");
+const start=document.getElementById("imgstart");
+
+gb.onclick=()=>{
+    location.href="./what.html";
+};
+
+sol.onclick=()=>{
+    location.href="./solution.html"
+}
+
+start.onclick=()=>{
+    location.href="./quiz.html"
+}
